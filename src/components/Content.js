bc1ed88dfd0e83714454aa4d37a8feb19d5bed6c
Shemuel2060 <2060newty@gmail.com>
2024-01-31 17:12:04 +0300

    made the search form a controlled component

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,15 +5,15 @@ import scientistData from '../assets/data/scientistData.json';
 const Content = () =>{
     return(
         <>
-            {scientistData.scientists.map((scientist,index)=>(
-                <div className='main-container'>
+            {scientistData.scientists.map((scientist)=>(
+                <div className='main-container' key={scientist.id}>
                   <div className='img-viewer'>
                         <img src={scientist.imageUrl} alt={scientist.alt}/>
                         <h4>{scientist.name}</h4>
                   </div>
                    <div className='biography'>
-                    <h3>{scientist.periodOfLife}</h3>
-                    <p>{scientist.work}</p>
+                        <h3>{scientist.periodOfLife}</h3>
+                        <p>{scientist.work}</p>
                    </div>
                 </div>
             ))}
